ae1a4d5e2ad51b8bf00f4c4fdba77342b922a1ec
Felipe Chatalov <fechatalov@gmail.com>
2024-03-04 13:36:01 -0300

    Refatorado

--- movieAnalisis.py
+++ movieAnalisis.py
@@ -11,26 +11,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB,MultinomialNB,BernoulliNB
 from sklearn.metrics import accuracy_score
 
+from sklearn.model_selection import train_test_split
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import re # for regex
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+
 # Carregando o dataset para treinamento
 data = pd.read_csv('IMDB-Dataset.csv')
-# print(data.shape)
-data.head()
 
-data.sentiment.value_counts()
 
+# substitui todos positivo/negativo por 0/1
 data.sentiment.replace('positive',1,inplace=True)
 data.sentiment.replace('negative',0,inplace=True)
-data.head(10)
 
-data.review[0]
 
+# usa regex para limpar o texto
+# tira tags html
 def clean(text):
     cleaned = re.compile(r'<.*?>')
+    # substitui o pattern por '' na string text e retorna a string substituida
     return re.sub(cleaned,'',text)
-
-data.review = data.review.apply(clean)
-data.review[0]
-
+# remove caracteres especiais
 def is_special(text):
     rem = ''
     for i in text:
@@ -39,43 +54,35 @@ def is_special(text):
         else:
             rem = rem + ' '
     return rem
-
-data.review = data.review.apply(is_special)
-data.review[0]
-
+# coloca o texto todo em minusculo
 def to_lower(text):
     return text.lower()
-
-data.review = data.review.apply(to_lower)
-data.review[0]
-
+# remove as stopwords, palavras que não tem significado
 def rem_stopwords(text):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text)
     return [w for w in words if w not in stop_words]
-
-data.review = data.review.apply(rem_stopwords)
-data.review[0]
-
+# ???????????????
 def stem_txt(text):
     ss = SnowballStemmer('english')
     return " ".join([ss.stem(w) for w in text])
 
+data.review = data.review.apply(clean)
+data.review = data.review.apply(to_lower)
+data.review = data.review.apply(rem_stopwords)
 data.review = data.review.apply(stem_txt)
-data.review[0]
 
-data.head()
 
-X = np.array(data.iloc[:,0].values)
+# y é um array com as sentimentos
 y = np.array(data.sentiment.values)
+# converte o texto em uma matriz de contagem de tokens (??)
 cv = CountVectorizer(max_features = 1000)
+
+# treino seguido de uma transformacao
 X = cv.fit_transform(data.review).toarray()
-print("X.shape = ",X.shape)
-print("y.shape = ",y.shape)
+
 
 trainx,testx,trainy,testy = train_test_split(X,y,test_size=0.2,random_state=9)
-print("Train shapes : X = {}, y = {}".format(trainx.shape,trainy.shape))
-print("Test shapes : X = {}, y = {}".format(testx.shape,testy.shape))
 
 gnb,mnb,bnb = GaussianNB(),MultinomialNB(alpha=1.0,fit_prior=True),BernoulliNB(alpha=1.0,fit_prior=True)
 gnb.fit(trainx,trainy)
--- numberClass.py
+++ numberClass.py
@@ -14,27 +14,22 @@ from sklearn.neural_network import BernoulliRBM
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 
-
 # Carregando o dataset para treinamento
 data = pd.read_csv('IMDB-Dataset.csv')
-# print(data.shape)
-data.head()
 
-data.sentiment.value_counts()
 
+# substitui todos positivo/negativo por 0/1
 data.sentiment.replace('positive',1,inplace=True)
 data.sentiment.replace('negative',0,inplace=True)
-data.head(10)
 
-data.review[0]
 
+# usa regex para limpar o texto
+# tira tags html
 def clean(text):
     cleaned = re.compile(r'<.*?>')
+    # substitui o pattern por '' na string text e retorna a string substituida
     return re.sub(cleaned,'',text)
-
-data.review = data.review.apply(clean)
-data.review[0]
-
+# remove caracteres especiais
 def is_special(text):
     rem = ''
     for i in text:
@@ -43,43 +38,35 @@ def is_special(text):
         else:
             rem = rem + ' '
     return rem
-
-data.review = data.review.apply(is_special)
-data.review[0]
-
+# coloca o texto todo em minusculo
 def to_lower(text):
     return text.lower()
-
-data.review = data.review.apply(to_lower)
-data.review[0]
-
+# remove as stopwords, palavras que não tem significado
 def rem_stopwords(text):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text)
     return [w for w in words if w not in stop_words]
-
-data.review = data.review.apply(rem_stopwords)
-data.review[0]
-
+# ???????????????
 def stem_txt(text):
     ss = SnowballStemmer('english')
     return " ".join([ss.stem(w) for w in text])
 
+data.review = data.review.apply(clean)
+data.review = data.review.apply(to_lower)
+data.review = data.review.apply(rem_stopwords)
 data.review = data.review.apply(stem_txt)
-data.review[0]
 
-data.head()
 
 X = np.array(data.iloc[:,0].values)
 y = np.array(data.sentiment.values)
+
 cv = CountVectorizer(max_features = 1000)
 X = cv.fit_transform(data.review).toarray()
-print("X.shape = ",X.shape)
-print("y.shape = ",y.shape)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-print("Train shapes : X = {}, y = {}".format(X_train.shape,Y_train.shape))
-print("Test shapes : X = {}, y = {}".format(X_test.shape,Y_test.shape))
+
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=0)
+print("Train shapes : X = {}, y = {}".format(train_x.shape,train_y.shape))
+print("Test shapes : X = {}, y = {}".format(test_x.shape,test_y.shape))
 
 logistic = linear_model.LogisticRegression(solver="newton-cg", tol=1)
 rbm = BernoulliRBM(random_state=0, verbose=True)
@@ -87,8 +74,8 @@ rbm = BernoulliRBM(random_state=0, verbose=True)
 rbm_features_classifier = Pipeline(steps=[("rbm", rbm), ("logistic", logistic)])
 
 # Training RBM-Logistic Pipeline
-rbm_features_classifier.fit(X_train, Y_train)
+rbm_features_classifier.fit(train_x, train_y)
 
-seila = rbm_features_classifier.predict(X_test)
+seila = rbm_features_classifier.predict(test_x)
 
-print("Sei la: ", accuracy_score(Y_test, seila))
+print("Sei la: ", accuracy_score(test_y, seila))
